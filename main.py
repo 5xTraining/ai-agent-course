f8939ef99510ff5fc0c62c03fbe1acc55641577f
Eddie Kao <eddie@5xcampus.com>
2025-02-22 00:22:09 +0800

    feat: add continuous conversation

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from config import OPENAI_API_KEY
 from openai import OpenAI
 
 client = OpenAI(api_key=OPENAI_API_KEY)
-user_question = input("請輸入你的問題：")
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",  # 選擇便宜的模型
-    messages=[
-        {
-            "role": "developer",
-            "content": "你是一位專門講關於貓的笑話大師，回答問題時請一律使用**繁體中文**",
-        },
-        {
-            "role": "user",
-            "content": user_question,
-        },
-    ],
-)
+try:
+    while True:
+        user_question = input("請輸入你的問題(輸入 exit 可結束對話)：")
 
-print(response.choices[0].message.content)
+        if user_question.lower() == "exit":
+            print("Bye!")
+            break
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",  # 選擇便宜的模型
+            messages=[
+                {
+                    "role": "developer",
+                    "content": "你是一位專門講關於貓的笑話大師，回答問題時請一律使用**繁體中文**",
+                },
+                {
+                    "role": "user",
+                    "content": user_question,
+                },
+            ],
+        )
+
+        print(response.choices[0].message.content)
+except EOFError:
+    print("Bye!")
