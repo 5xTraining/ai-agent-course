020df1a1361fdfa039649f04ea851d86a78e9a77
Eddie Kao <eddie@5xcampus.com>
2025-06-21 06:09:35 +0800

    feat: add message history

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from config import OPENAI_API_KEY
 from openai import OpenAI
 from prompt_toolkit import prompt
+from db.messages import init_message, add_message, get_messages
 
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+init_message("你是一位專門講關於貓的笑話大師，回答問題一律使用**台灣繁體中文**")
 
 try:
     while True:
@@ -13,20 +16,16 @@ try:
                 print("再會~")
                 break
 
+            add_message(user_question)
+
             completion = client.chat.completions.create(
                 model="gpt-4.1-nano",  # 選擇便宜的模型
-                messages=[
-                    {
-                        "role": "developer",
-                        "content": "你是一位專門講關於貓的笑話大師，回答問題一律使用**台灣繁體中文**",
-                    },
-                    {
-                        "role": "user",
-                        "content": user_question,
-                    },
-                ],
+                messages=get_messages(),
             )
 
-            print(completion.choices[0].message.content)
+            content = completion.choices[0].message.content
+
+            add_message(content, role="assistant")  # 把 LLM 的回應也存起來
+            print(content)
 except EOFError:
     print("再會~")
